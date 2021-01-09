eb6d9a901d381c88283246d900023d76424614a5
AnadyaNairofficial✔ <71205867+AnadyaNair@users.noreply.github.com>
2021-01-09 23:28:31 +0530

    Automatic Whatsapp message through Python

---
+++ automessage.py
@@ -0,0 +1,9 @@
+# Code from https://github.com/AnadyaNair 
+# pip install pywhatkit 
+# this statement ☝🏻 will install the pywhatkit package in your computer.
+# it will only work if your pip commands are enabled.
+# and while running the program, ensure that your browser is open and your Whatsapp web is connected.
+import pywhatkit
+pywhatkit.sendwhatmsg('ENTER THE PHONE NUMBER HERE ON WHICH THE MESSAGE HAS TO BE SENT', 'ENTER YOUR MESSAGE, i.e. Hi! The pywhatkit pacakge works!', ENTER THE HOUR, ENTER THE MINUTE)
+
+#THANK YOU!
